0a2fc303c21d8976e99d552bc1455c1e3b1b51d7
MaartenVanNimwgen <maartenvannimwegen@hotmail.com>
2023-09-13 11:39:57 +0200

    login en register system added

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,58 @@
+<?php
+ 
+namespace App\Http\Controllers;
+
+use app\Http\Middleware\AdminMiddleware;
+use App\Models\User;
+use GuzzleHttp\Middleware;
+use Illuminate\Http\Request;
+use Illuminate\Routing\MiddlewareNameResolver;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+ 
+class AuthController extends Controller
+{
+    public function register()
+    {
+        return view('register');
+    }
+ 
+    public function registerPost(Request $request)
+    {
+        $user = new User();
+ 
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->role = $request->role;
+ 
+        $user->save();
+ 
+        return back()->with('success', 'Register successfully');
+    }
+ 
+    public function login()
+    {
+        return view('login');
+    }
+ 
+    public function loginPost(Request $request)
+    {
+        $credetials = [
+            'email' => $request->email,
+            'password' => $request->password,
+        ];
+ 
+        if (Auth::attempt($credetials)) {
+            return redirect('/home')->with('success', 'Login Success');
+        }
+ 
+        return back()->with('error', 'Error Email or Password');
+    }
+ 
+    public function logout()
+    {
+        Auth::logout();
+ 
+        return redirect()->route('login');
+    }
+}
