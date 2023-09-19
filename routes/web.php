87fff2f5bf6ae8a828b863f21e650983b3be45f8
MaartenVanNimwgen <maartenvannimwegen@hotmail.com>
2023-09-19 21:48:38 +0200

    overzichtelijker

--- routes/web.php
+++ routes/web.php
@@ -12,31 +12,22 @@ Route::get('/', function () {
     return view('login');
 });
 
-// Wachtwoord
 Route::get('/wachtwoord', [PasswordController::class, 'showPasswordForm'])->name('password.form');
 Route::post('/wachtwoord', [PasswordController::class, 'setPassword'])->name('set-password');
 
 Route::group(['middleware' => 'guest'], function () {
-    // Login
     Route::get('/login', [AuthController::class, 'login'])->name('login');
     Route::post('/login', [AuthController::class, 'loginPost'])->name('login');
-
 });
 
 Route::group(['middleware' => 'auth'], function () {
-    // Home
     Route::get('/home', [HomeController::class, 'index'])->name('home');
-
-    // Logout
     Route::delete('/logout', [AuthController::class, 'logout'])->name('logout');
 });
 
 Route::group(['middleware' => AdminMiddleware::class], function () {
-    // Register
     Route::get('/register', [AuthController::class, 'register'])->name('register');
     Route::post('/register', [AuthController::class, 'registerPost'])->name('register');
-
-    // Add Lesson
     Route::get('/addLes', [LesController::class, 'addLes'])->name('index');
     Route::post('/addLes', [LesController::class, 'addLesPost'])->name('addLes');
 });
