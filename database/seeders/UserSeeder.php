3c603d43aef24c03f1699408be2f79a59f2b9c00
MaartenVanNimwgen <maartenvannimwegen@hotmail.com>
2023-09-19 20:21:10 +0200

    seeders

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,12 +14,55 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        // ADMINS
         DB::table('users')->insert([
-            'name' => 'Maarten',
-            'email' => 'maarten@gmail.com',
+            'name' => 'Frans de Boer',
+            'email' => 'fdeboer@rocfriesepoort.nl',
             'password' => '$2y$10$dKgjhq4xiXt1aKZoN/L9IePt4GAzZ2zSSf87DjWmXAo4YdwjzNFde',
             'password_code' => Hash::make('test'),
             'role' => 'admin',
         ]);
+
+        DB::table('users')->insert([
+            'name' => 'Richard Kingma',
+            'email' => 'rkingma@rocfriesepoort.nl',
+            'password' => '$2y$10$dKgjhq4xiXt1aKZoN/L9IePt4GAzZ2zSSf87DjWmXAo4YdwjzNFde',
+            'password_code' => Hash::make('test'),
+            'role' => 'admin',
+        ]);
+
+
+        // STUDENTS
+        DB::table('users')->insert([
+            'name' => 'Maarten van Nimwegen',
+            'email' => 'maarten@gmail.com',
+            'password' => '$2y$10$dKgjhq4xiXt1aKZoN/L9IePt4GAzZ2zSSf87DjWmXAo4YdwjzNFde',
+            'password_code' => Hash::make('test'),
+            'role' => 'student',
+        ]);   
+        
+        DB::table('users')->insert([
+            'name' => 'Tim Hammersma',
+            'email' => 'tim@gmail.com',
+            'password' => '$2y$10$dKgjhq4xiXt1aKZoN/L9IePt4GAzZ2zSSf87DjWmXAo4YdwjzNFde',
+            'password_code' => Hash::make('test'),
+            'role' => 'student',
+        ]);   
+
+        DB::table('users')->insert([
+            'name' => 'Kevin Kamstra',
+            'email' => 'kevin@gmail.com',
+            'password' => '$2y$10$dKgjhq4xiXt1aKZoN/L9IePt4GAzZ2zSSf87DjWmXAo4YdwjzNFde',
+            'password_code' => Hash::make('test'),
+            'role' => 'student',
+        ]);   
+
+        DB::table('users')->insert([
+            'name' => 'Christian Koopman',
+            'email' => 'christian@gmail.com',
+            'password' => '$2y$10$dKgjhq4xiXt1aKZoN/L9IePt4GAzZ2zSSf87DjWmXAo4YdwjzNFde',
+            'password_code' => Hash::make('test'),
+            'role' => 'student',
+        ]);   
     }
 }
