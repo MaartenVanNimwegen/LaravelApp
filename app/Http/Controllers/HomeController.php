624e69aca44e15266ffcc6951369458a79e28d37
MaartenVanNimwgen <maartenvannimwegen@hotmail.com>
2023-09-21 14:13:40 +0200

    Les inschrijven gefixt

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,10 +1,11 @@
 <?php
- 
+
 namespace App\Http\Controllers;
- 
+
+use App\Models\Les_user_koppel;
 use Illuminate\Http\Request;
 use App\Models\Les;
- 
+
 class HomeController extends Controller
 {
     public function index()
@@ -12,5 +13,17 @@ class HomeController extends Controller
         $lesController = new LesController();
         $upcommingLessons = $lesController->ViewAllComingLessons();
         return view('home', compact('upcommingLessons'));
+    }
+
+    public function isUserAangemeld($lesId)
+    {
+        $userId = auth()->user()->id;
+        $records = Les_user_koppel::where('userId', $userId)
+        ->where('lesId', $lesId)
+        ->get();
+        if ($records->isEmpty()) {
+            return false;
+        }
+        return true;
     }
 }
---
+++ app/Http/Controllers/helpers.php
@@ -0,0 +1,14 @@
+<?php
+use App\Models\Les_user_koppel;
+
+function isUserAangemeld($lesId)
+{
+    $userId = auth()->user()->id;
+    $records = Les_user_koppel::where('userId', $userId)
+        ->where('lesId', $lesId)
+        ->get();
+    if ($records->isEmpty()) {
+        return false;
+    }
+    return true;
+}
