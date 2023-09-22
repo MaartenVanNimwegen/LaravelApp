4fc00b3ce6901f0aa5472568b7fecbe36986c09f
MaartenVanNimwgen <maartenvannimwegen@hotmail.com>
2023-09-22 20:27:44 +0200

    Remove using

--- app/Http/Controllers/VraagController.php
+++ app/Http/Controllers/VraagController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Vraag;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Validation\Rules\Password;
 
 class VraagController extends Controller
 {
