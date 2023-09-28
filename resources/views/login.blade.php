3fd449269207361c4fff3f7bee46b8c200abf8e6
MaartenVanNimwgen <maartenvannimwegen@hotmail.com>
2023-09-28 13:05:43 +0200

    voorbeeld

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -40,9 +40,9 @@
                     <form action="{{ route('login') }}" method="POST">
                         @csrf
                         <div class="mb-3">
-                            <label for="email" class="form-label">Email-address</label>
+                            <label for="email" class="form-label">E-mailadres</label>
                             <input type="email" name="email" class="form-control" id="email"
-                                placeholder="name@example.com" required>
+                                placeholder="naam@voorbeeld.com" required>
                         </div>
                         <div class="mb-3">
                             <label for="password" class="form-label">wachtwoord</label>
